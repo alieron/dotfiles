12c70b24ccf131fafab59070d4c0b2e4a737617b
alieron <sebastien.leib1@gmail.com>
2025-06-27 03:10:43 +0800

    Fix tab indentation inconsistencies

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,8 @@ require("config.lazy")
 --- Options ---
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true

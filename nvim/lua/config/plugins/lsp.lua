46b4cdd43090ab96f83e325be9481f4ecc199517
Sebastien Leib <sebastien.leib1@gmail.com>
2026-05-15 12:39:11 +0800

    Update lspconfig configuration

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,7 +1,7 @@
 -- Format/rename/go to reference with lsp
 vim.keymap.set('n', 'gf', vim.lsp.buf.format)
 vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
-vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+-- vim.keymap.set('n', 'gra', vim.lsp.buf.code_action) -- default in 0.11
 vim.keymap.set('n', 'grr', vim.lsp.buf.references)
 
 return {
@@ -20,31 +20,48 @@ return {
     },
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
 
-      lspconfig.basedpyright.setup({
+      -- Set capabilities and shared defaults for all servers
+      vim.lsp.config('*', {
         capabilities = capabilities,
+      })
+
+      -- Servers with custom config
+      vim.lsp.config('basedpyright', {
+        single_file_support = true,
+        settings = {
+          basedpyright = {
+            analysis = {
+              typeCheckingMode = "basic",
+            },
+          },
+        },
+      })
+
+      vim.lsp.config('ruff', {
         single_file_support = true,
       })
 
-      lspconfig.ruff.setup({
-        capabilities = capabilities,
-        single_file_support = true,
+      -- Enable all servers
+      vim.lsp.enable({
+        'basedpyright',
+        'ruff',
+        'lua_ls',
+        'clangd',
+        'jsonls',
+        'ts_ls',
+        'astro',
+        'jdtls',
+        'svelte',
+        'rust_analyzer',
       })
 
-      local servers = { 'lua_ls', 'clangd', 'jsonls', 'ts_ls', 'astro', 'jdtls' }
-
-      for _, lsp in pairs(servers) do
-        lspconfig[lsp].setup { capabilities = capabilities }
-      end
-
+      -- Auto-format Lua files on save
       vim.api.nvim_create_autocmd('LspAttach', {
         callback = function(args)
           local c = vim.lsp.get_client_by_id(args.data.client_id)
           if not c then return end
-
           if vim.bo.filetype == "lua" then
-            -- Format the current buffer on save
             vim.api.nvim_create_autocmd('BufWritePre', {
               buffer = args.buf,
               callback = function()
